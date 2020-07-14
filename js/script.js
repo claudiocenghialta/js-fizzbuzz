feb979d6bebdb29bf2de17ff38faf23f04187ce1
Claudio Cenghialta <claudio.cenghialta@gmail.com>
2020-07-14 14:56:51 +0200

    es completato

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/*
+Scrivi un programma che stampi i numeri da 1 a 100,
+ma per i multipli di 3 stampi “Fizz” al posto del numero
+ e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+*/
+var risultato
+for (i=1;i<=100;i++) {
+    if ((i%5==0)&&(i%3==0)){
+        risultato='FizzBuzz';
+    } else if (i%5==0) {
+        risultato='Buzz';
+    } else if (i%3==0) {
+        risultato='Fizz';
+    } else {
+        risultato=i;
+    }
+    document.getElementById('elenco').innerHTML+='<li>'+risultato+'</li>';
+}
